cec893aedeb860aa6100a0e13599e5444175357e
Ridzki R Alfaza <ridzkiraihanalfaza@gmail.com>
2022-11-14 17:21:37 +0700

    no 345 proxy

--- Berlint/script.sh
+++ Berlint/script.sh
@@ -22,13 +22,22 @@ visible_hostname Berlint
 
 http_access allow AVAILABLE_WORKING
 http_access allow OUT_WORKING
+http_access deny !SSL_ports
+
+delay_pools 1
+delay_class 1 2
+delay_access 1 allow all !NO_LIMIT_DAYS
+delay_parameters 1 none 16000/16000
 ' > /etc/squid/squid.conf
 
 echo '
+acl SSL_ports port 443
 acl AVAILABLE_WORKING time AS 00:00-23:59
 acl AVAILABLE_WORKING time MTWHF 00:00-07:59
 acl AVAILABLE_WORKING time MTWHF 17:00-23:59
 acl OUT_WORKING dstdomain .loid-work.com .ranky-work.com
+acl NO_LIMIT_DAYS time MTWHF
+
 ' > /etc/squid/acl.conf
 
 service squid restart
